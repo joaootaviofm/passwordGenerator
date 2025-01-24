220201610e17ac5ca50e9a08ca9eef7952cb221c
joaootaviofm <joaootaviofm@gmail.com>
2025-01-24 11:07:55 -0300

    const button adicionada e arrow function iniciada

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ const symbolBox = document.getElementById('checkSymbol')
 const numberBox = document.getElementById('checkNumber')
 const upperBox = document.getElementById('checkUpper')
 const lowerBox = document.getElementById('checkLower')
+const generateButton = document.getElementById('generateButton')
+
+
+generateButton.addEventListener('click', () =>{
+    
+})
