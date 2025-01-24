324418ae2ea635d483a39fd580c82691efebb2e6
joaootaviofm <joaootaviofm@gmail.com>
2025-01-24 11:03:03 -0300

    criação de variáveis

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const passwordLength = document.getElementById('inputPasswordLength')
+const symbolBox = document.getElementById('checkSymbol')
+const numberBox = document.getElementById('checkNumber')
+const upperBox = document.getElementById('checkUpper')
+const lowerBox = document.getElementById('checkLower')
