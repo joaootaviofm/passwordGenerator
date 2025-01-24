97fc06659d78a48d71b57902331bf76029f84e37
joaootaviofm <joaootaviofm@gmail.com>
2025-01-24 11:14:47 -0300

    criação das variáveis para armazenar os caracteres

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,10 @@ const upperBox = document.getElementById('checkUpper')
 const lowerBox = document.getElementById('checkLower')
 const generateButton = document.getElementById('generateButton')
 
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789"
+const symbols = "~!@#$%^&*()_+"
 
 generateButton.addEventListener('click', () =>{
     
